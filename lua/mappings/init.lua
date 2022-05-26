041b4f9e1fbfa067e1519d45ef4a4ee95b7e4a2b
Hanan Avramovich <hanan.avramovich@ex.co>
2022-05-26 22:37:00 +0300

    added telescope keybindings

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -5,3 +5,7 @@ vim.g.mapleader = ' '
 
 map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
 
+map('n', '<leader>ff', '<cmd>lua require("telescope.builtin").find_files()<cr>', opts)
+map('n', '<leader>fg', '<cmd>lua require("telescope.builtin").live_grep()<cr>', opts)
+map('n', '<leader>fb', '<cmd>lua require("telescope.builtin").buffers()<cr>', opts)
+map('n', '<leader>fh', '<cmd>lua require("telescope.builtin").help_tags()<cr>', opts)
