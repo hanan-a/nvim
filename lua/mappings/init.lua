1d6a8743186ad40008b282f4cf3ce78a633f54ff
Hanan Avramovich <hanan.avramovich@ex.co>
2022-05-30 00:21:02 +0300

    added nvim-gomove keys

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -21,3 +21,23 @@ map('v', '<leader>n', "<cmd> lua require'hop'.hint_words({ hint_position = requi
 map('o', '<leader>n', "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.END, inclusive_jump = true })<cr>", {})
 
 map('n', '<leader>lg', ':LazyGit<CR>', opts)
+
+map( "n", "<S-h>", "<Plug>GoNSMLeft", {} )
+map( "n", "<S-j>", "<Plug>GoNSMDown", {} )
+map( "n", "<S-k>", "<Plug>GoNSMUp", {} )
+map( "n", "<S-l>", "<Plug>GoNSMRight", {} )
+
+map( "x", "<S-h>", "<Plug>GoVSMLeft", {} )
+map( "x", "<S-j>", "<Plug>GoVSMDown", {} )
+map( "x", "<S-k>", "<Plug>GoVSMUp", {} )
+map( "x", "<S-l>", "<Plug>GoVSMRight", {} )
+
+map( "n", "<C-h>", "<Plug>GoNSDLeft", {} )
+map( "n", "<C-j>", "<Plug>GoNSDDown", {} )
+map( "n", "<C-k>", "<Plug>GoNSDUp", {} )
+map( "n", "<C-l>", "<Plug>GoNSDRight", {} )
+
+map( "x", "<C-h>", "<Plug>GoVSDLeft", {} )
+map( "x", "<C-j>", "<Plug>GoVSDDown", {} )
+map( "x", "<C-k>", "<Plug>GoVSDUp", {} )
+map( "x", "<C-l>", "<Plug>GoVSDRight", {} )
