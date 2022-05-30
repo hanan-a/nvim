3f6c8d30bc5d7f06965b9865c324ebe574941639
Hanan Avramovich <hanan.avramovich@ex.co>
2022-05-30 14:49:08 +0300

    added ;; as Esc and z, ff to telescope

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -2,11 +2,12 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 vim.g.mapleader = ' '
+map('i', ';;', '<Esc>', opts)
 
 map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
 
-map('n', '<leader>ff', '<cmd>lua require("telescope.builtin").find_files()<cr>', opts)
-map('n', '<leader>fg', '<cmd>lua require("telescope.builtin").live_grep()<cr>', opts)
+map('n', '<leader>z', '<cmd>lua require("telescope.builtin").find_files()<cr>', opts)
+map('n', '<leader>f', '<cmd>lua require("telescope.builtin").live_grep()<cr>', opts)
 map('n', '<leader>fb', '<cmd>lua require("telescope.builtin").buffers()<cr>', opts)
 map('n', '<leader>fh', '<cmd>lua require("telescope.builtin").help_tags()<cr>', opts)
 
