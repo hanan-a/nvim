17f5c7fd6337c9c08d4f731904dc5a170d74c103
Hanan Avramovich <hanan.avramovich@ex.co>
2022-07-31 22:06:49 +0300

    telescope: dropdown theme

--- lua/telescope-config/init.lua
+++ lua/telescope-config/init.lua
@@ -1,1 +1,12 @@
 require'telescope'.load_extension('project')
+
+require('telescope').setup{
+  pickers = {
+    find_files = {
+      theme = "dropdown",
+    },
+    live_grep = {
+      theme = "dropdown",
+    }
+  }
+}
