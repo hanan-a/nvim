3cdb49393e1a87af5e90bdbc6a1dff92622c314d
Hanan Avramovich <hanan.avramovich@ex.co>
2022-08-02 00:30:05 +0300

    fix: markdown preview

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -111,7 +111,10 @@ return require('packer').startup(function()
       requires = {'kyazdani42/nvim-web-devicons'}
     }
     -- Markdown Preview
-    use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+    use({
+      "iamcco/markdown-preview.nvim",
+      run = function() vim.fn["mkdp#util#install"]() end,
+    })
     use {
       "folke/trouble.nvim",
       requires = "kyazdani42/nvim-web-devicons",
