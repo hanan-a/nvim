b19c8ce5e858a6285426b08a8301151250d7b66c
Hanan Avramovich <hananavramovich@gmail.com>
2023-04-25 11:04:26 +0300

    WIP DAP: Added CPP/Rust configurations

--- lua/core/plugin_config/nvim-dap.lua
+++ lua/core/plugin_config/nvim-dap.lua
@@ -70,6 +70,37 @@ for _, language in ipairs({ "typescript", "javascript" }) do
     }
 end
 
+dap.adapters.cppdbg = {
+  id = 'cppdbg',
+  type = 'executable',
+  command = '/Users/hananavramovich/.vscode/extensions/ms-vscode.cpptools-1.14.5-darwin-arm64/debugAdapters/bin ',
+}
+
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "cppdbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopAtEntry = true,
+  },
+  {
+    name = 'Attach to gdbserver :1234',
+    type = 'cppdbg',
+    request = 'launch',
+    MIMode = 'gdb',
+    miDebuggerServerAddress = 'localhost:1234',
+    miDebuggerPath = '/usr/bin/gdb',
+    cwd = '${workspaceFolder}',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+  },
+}
+
 local dapui =  require("dapui")
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open()
