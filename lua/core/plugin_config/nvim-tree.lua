5e599d0227b41608744444912f1847f7633038d8
Hanan Avramovich <hananavramovich@gmail.com>
2023-04-28 23:04:11 +0300

    Better nvim-tree: perserve split dimensions & diagnostics

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -1,6 +1,21 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require('nvim-tree').setup()
+require('nvim-tree').setup({
+  view = {
+    preserve_window_proportions = true,
+    float = {
+      enable = false,
+      open_win_config = {
+        border = "rounded",
+        width = 30,
+        height = 50,
+      }
+    },
+  },
+  diagnostics = {
+    enable = true
+  }
+})
 
 vim.keymap.set('n', '<C-n>', ':NvimTreeFindFileToggle<CR>')
