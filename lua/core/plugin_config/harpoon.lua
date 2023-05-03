4b45ab6663f601146395decab40aa734f39f0870
Hanan Avramovich <hananavramovich@gmail.com>
2023-05-03 21:59:00 +0300

    updated harpoon keymaps

--- lua/core/plugin_config/harpoon.lua
+++ lua/core/plugin_config/harpoon.lua
@@ -1,7 +1,12 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
+require("telescope").load_extension('harpoon')
 
-map('n', '<C-e>', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', opts)
+map('n', '<C-h>', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', opts)
+map('n', '<C-e>', ':Telescope harpoon marks initial_mode=normal<CR>', opts)
 map('n', '<leader>m', ':lua require("harpoon.mark").add_file()<CR>', opts)
 map('n', '<leader>mr', ':lua require("harpoon.mark").rm_file()<CR>', opts)
+map('n', '<leader>n', ':lua require("harpoon.mark").rm_file()<CR>', opts)
+map('n', '<leader>o', ':lua require("harpoon.ui").nav_next()<CR>', opts)
+map('n', '<leader>i', ':lua require("harpoon.ui").nav_prev()<CR>', opts)
