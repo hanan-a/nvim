e6c2ebff8d1c9fb34faf857dd14abd2b1eeda6bb
Hanan Avramovich <hananavramovich@gmail.com>
2023-05-30 08:12:40 +0300

    updated harpoon next/prev to C-c/C-x

--- lua/core/plugin_config/harpoon.lua
+++ lua/core/plugin_config/harpoon.lua
@@ -3,9 +3,9 @@ local opts = { noremap = true, silent = true }
 
 require("telescope").load_extension('harpoon')
 
--- map('n', '<C-h>', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', opts)
-map('n', '<C-e>', ':Telescope harpoon marks initial_mode=normal<CR>', opts)
+map('n', '<C-e>', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', opts)
+-- map('n', '<C-e>', ':Telescope harpoon marks initial_mode=normal previewer=false<CR>', opts)
 map('n', '<leader>m', ':lua require("harpoon.mark").add_file()<CR>', opts)
 map('n', '<leader>mr', ':lua require("harpoon.mark").rm_file()<CR>', opts)
-map('n', '<leader>o', ':lua require("harpoon.ui").nav_next()<CR>', opts)
-map('n', '<leader>i', ':lua require("harpoon.ui").nav_prev()<CR>', opts)
+map('n', '<C-c>', ':lua require("harpoon.ui").nav_next()<CR>', opts)
+map('n', '<C-x>', ':lua require("harpoon.ui").nav_prev()<CR>', opts)
