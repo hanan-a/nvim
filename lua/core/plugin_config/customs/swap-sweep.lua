a7ec987005e2c27cceca4f09e807029b0ef75b10
Hanan Avramovich <hananavramovich@gmail.com>
2023-12-18 09:11:30 +0200

    linting swap-sweep.lua

--- lua/core/plugin_config/customs/swap-sweep.lua
+++ lua/core/plugin_config/customs/swap-sweep.lua
@@ -11,8 +11,6 @@ local swap_file_dir = os.getenv('HOME') .."/.local/state/nvim/swap/"
 
 local S = {}
 S.find_and_sweep = function()
-
-
   local telescope_opts = require('telescope.themes').get_dropdown({
     prompt_title = "Delete Swap File",
     previewer = false,
@@ -20,25 +18,20 @@ S.find_and_sweep = function()
   })
 
   telescope_opts['initial_mode'] = 'normal'
-
   telescope_opts["attach_mappings"] = function(prompt_bufnr)
     actions.select_default:replace(function()
       actions.close(prompt_bufnr)
-
       local selection = action_state.get_selected_entry()
       local file_name = selection[1]
       os.remove(swap_file_dir ..file_name)
       print('Swap file deleted: ' ..file_name)
     end)
-
     return true
   end
-
   require('telescope.builtin').find_files(telescope_opts)
 end
 
 S.delete_all = function ()
-  
 end
 
 return S
