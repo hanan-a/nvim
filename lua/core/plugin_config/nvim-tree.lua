86ba404aac0625606cc6d0d7fbd3d92172d9ca6c
hanan-a <hanan.avramovich@ex.co>
2024-02-25 14:44:01 +0200

    feat(nvim-tree): don't filter git ignored files

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -6,15 +6,22 @@ require('nvim-tree').setup({
     preserve_window_proportions = true,
     float = {
       enable = false,
+      quit_on_focus_loss = true,
       open_win_config = {
         border = "rounded",
-        width = 30,
+        width = 40,
         height = 50,
+        row = 1,
+        col = 1,
       }
     },
   },
   diagnostics = {
     enable = true
+  },
+  filters = {
+    dotfiles = false,
+    git_ignored = false,
   }
 })
 
