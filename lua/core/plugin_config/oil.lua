a73c6ab7526c9d93334aa8511693036cf4742e97
hanan-a <hanan.avramovich@ex.co>
2024-05-24 00:11:50 +0300

    feat(oil): open as float

--- lua/core/plugin_config/oil.lua
+++ lua/core/plugin_config/oil.lua
@@ -1,3 +1,28 @@
-require("oil").setup()
+require("oil").setup({
+  -- git = {
+  --   -- Return true to automatically git add/mv/rm files
+  --   add = function(path)
+  --     return false
+  --   end,
+  --   mv = function(src_path, dest_path)
+  --     return true
+  --   end,
+  --   rm = function(path)
+  --     return true
+  --   end,
+  -- },
+  -- Configuration for the floating window in oil.open_float
+  float = {
+    -- Padding around the floating window
+    padding = 2,
+    max_width = 0,
+    max_height = 0,
+    border = "rounded",
+    win_options = {
+      winblend = 0,
+    },
+  }
+})
 
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+-- vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "-", ":lua require('oil').open_float()<CR>", { desc = "Open parent directory" })
