cac0e56066f7fdd978dbd1ea2b34788e08f88f54
hanan-a <hanan.avramovich@ex.co>
2024-05-26 12:06:56 +0300

    feat(neogit): changed open method + `<leader>gb` telescope branches

--- lua/core/plugin_config/neogit.lua
+++ lua/core/plugin_config/neogit.lua
@@ -1,4 +1,6 @@
 local neogit = require('neogit')
 neogit.setup {}
 
-vim.keymap.set("n", "+", ":Neogit<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "+", neogit.open, { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>gb", ':Telescope git_branches<CR>', { noremap = true, silent = true })
