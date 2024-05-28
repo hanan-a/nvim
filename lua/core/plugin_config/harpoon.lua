78f2c06831e00e6589cebf5333a17ef42da4cd84
hanan-a <hanan.avramovich@ex.co>
2024-05-28 11:02:24 +0300

    feat(harpoon): keymap switch with Telescope activation

--- lua/core/plugin_config/harpoon.lua
+++ lua/core/plugin_config/harpoon.lua
@@ -10,11 +10,11 @@ local ui = require("harpoon.ui")
 local mark = require("harpoon.mark")
 
 -- map('n', '<C-m>', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', opts)
-vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu, opts)
+vim.keymap.set('n', '<leader>e', ui.toggle_quick_menu, opts)
 vim.keymap.set('n', '<leader>m', mark.add_file, opts)
 vim.keymap.set('n', '<leader>mr', mark.rm_file, opts)
 vim.keymap.set('n', '<C-c>', ui.nav_next, opts)
 vim.keymap.set('n', '<C-x>', ui.nav_prev, opts)
 
-vim.keymap.set('n', '<leader>e', ':Telescope harpoon marks initial_mode=normal<CR>', opts)
+vim.keymap.set('n', '<C-e>', ':Telescope harpoon marks initial_mode=normal<CR>', opts)
 -- vim.keymap.set('n', '<leader>e', ':Telescope harpoon marks initial_mode=normal previewer=false<CR>', opts)
