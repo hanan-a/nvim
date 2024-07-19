e519fa6a263db53eb0e9865b2e930b4408f316c1
hanan-a <hanan.avramovich@ex.co>
2024-07-19 17:45:11 +0300

    feat(neogit): added `gl` keymap for log

--- lua/core/plugin_config/neogit.lua
+++ lua/core/plugin_config/neogit.lua
@@ -2,5 +2,6 @@ local neogit = require('neogit')
 neogit.setup {}
 
 vim.keymap.set("n", "+", neogit.open, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>gl", function() neogit.open({ "log" }) end, { noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>gb", ':Telescope git_branches<CR>', { noremap = true, silent = true })
