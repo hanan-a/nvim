9292c1ba738f572c9e5905a11ff15276f84dc419
hanan-a <hanan.avramovich@ex.co>
2024-07-21 15:12:34 +0300

    fix(catppuccin): config on load

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,36 +1,37 @@
 return {
-  "catppuccin/nvim", 
+  "catppuccin/nvim",
   lazy = false,
-  name = "catppuccin", 
+  name = "catppuccin",
   priority = 1000,
-  opt = {
-    transparent_background = true, -- disables setting the background color.
-    integrations = {
-      cmp = true,
-      gitsigns = true,
-      nvimtree = true,
-      treesitter = true,
-      notify = false,
-      mini = {
-        enabled = true,
-        indentscope_color = "",
-      },
-      alpha = true,
-      barbecue = {
-        dim_dirname = true, -- directory name is dimmed by default
-        bold_basename = true,
-        dim_context = false,
-        alt_background = false,
-      },
-      telescope = {
-        enabled = true,
-        style = "nvchad",
-      },
-      --         -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-    },
-  },
   config = function()
+    require('catppuccin').setup({
+      transparent_background = true, -- disables setting the background color.
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+        alpha = true,
+        barbecue = {
+          dim_dirname = true, -- directory name is dimmed by default
+          bold_basename = true,
+          dim_context = false,
+          alt_background = false,
+        },
+        telescope = {
+          enabled = true,
+          style = "nvchad",
+        },
+        --         -- for more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+      },
+    })
+
     vim.cmd([[colorscheme catppuccin]])
-  end
+  end,
 }
 
