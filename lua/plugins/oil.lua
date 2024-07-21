55012034d888a203a3362b4b57010630a22cf7c2
hanan-a <hanan.avramovich@ex.co>
2024-07-21 22:34:39 +0300

    feat(oil): lazy-load

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,38 +1,26 @@
 return   {
   'stevearc/oil.nvim',
   opts = {
-    -- git = {
-     --   -- return true to automatically git add/mv/rm files
-      --   add = function(path)
-        --     return false
-        --   end,
-        --   mv = function(src_path, dest_path)
-          --     return true
-          --   end,
-          --   rm = function(path)
-            --     return true
-            --   end,
-            -- },
-            -- configuration for the floating window in oil.open_float
-            float = {
-              -- padding around the floating window
-              padding = 2,
-              max_width = 0,
-              max_height = 0,
-              border = "rounded",
-              win_options = {
-                winblend = 0,
-              },
-            },
-            view_options = {
-              show_hidden = true,
-            }
-          },
-          -- Optional dependencies
-          dependencies = { "nvim-tree/nvim-web-devicons" },
-          init = function()
-            local oil = require("oil")
-            vim.keymap.set("n", "-", oil.open, { desc = "Open parent directory" })
-            vim.keymap.set("n", "_", oil.open_float, { desc = "Open parent directory" })
-          end
-        }
+    -- configuration for the floating window in oil.open_float
+    float = {
+      -- padding around the floating window
+      padding = 2,
+      max_width = 0,
+      max_height = 0,
+      border = "rounded",
+      win_options = {
+        winblend = 0,
+      },
+    },
+    view_options = {
+      show_hidden = true,
+    }
+  },
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  cmd = "Oil",
+  keys = {
+    { "-", "<cmd>Oil<cr>", desc = "Open Oil Explorer" },
+    { "_", "<cmd>Oil --float<cr>", desc = "Open Oil Explorer" },
+  },
+}
