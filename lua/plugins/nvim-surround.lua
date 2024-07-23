d3aacfb2814f096d1e0f0be5b5712dbde28250c0
hanan-a <hanan.avramovich@ex.co>
2024-07-23 09:15:14 +0300

    feat(nvim-surround): updated config

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -1,6 +1,10 @@
-return   {
+return  {
   "kylechui/nvim-surround",
-  event = "VeryLazy",
   version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  event = "VeryLazy",
+  config = function()
+    require("nvim-surround").setup({
+      -- Configuration here, or leave empty to use defaults
+    })
+  end
 }
-
