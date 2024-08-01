acc45b615c143c53b7489928ed169cb096d4c7d6
hanan-a <hanan.avramovich@ex.co>
2024-08-01 23:09:43 +0300

    feat(tmux): resize by 10 cells

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,6 +1,15 @@
 return {
   'aserowy/tmux.nvim',
-  config = function()
-    require('tmux').setup()
-  end
+  opts = {
+    resize = {
+        -- enables default keybindings (A-hjkl) for normal mode
+        enable_default_keybindings = true,
+
+        -- sets resize steps for x axis
+        resize_step_x = 10,
+
+        -- sets resize steps for y axis
+        resize_step_y = 10,
+    }
+  },
 }
