cd1d7aab5335a7a41f404d82b274dc923ca5f9ed
hanan-a <hanan.avramovich@ex.co>
2024-08-10 11:48:23 +0300

    fix(nvim-treesitter): updated config to install all dependencies

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,33 @@
 return   {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate'
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function ()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = {
+        "json",
+        "javascript",
+        "typescript",
+        "tsx",
+        "yaml",
+        "html",
+        "css",
+        "markdown",
+        "markdown_inline",
+        "svelte",
+        "bash",
+        "lua",
+        "dockerfile",
+        "gitignore",
+        "rust",
+        "lua",
+        "python",
+        "svelte",
+      },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
 }
 
