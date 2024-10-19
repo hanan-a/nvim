173a5755e2cff6fb8a30075a9fa0b960ca2ef5b6
hanan-a <hanan.avramovich@ex.co>
2024-10-19 18:12:03 +0300

    fix(nvim-cmp): updated `table.unpack`

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -15,7 +15,7 @@ M.config = function()
   local luasnip = require 'luasnip'
 
   local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
   end
 
