08c3605fb266723239459902deed105ff73c528c
hanan-a <hanan.avramovich@ex.co>
2024-10-22 16:22:38 +0300

    fix(barbecue): lazy loading + exclusions

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -2,13 +2,24 @@ return {
   "utilyre/barbecue.nvim",
   enabled = true,
   version = "*",
+  event = { "VeryLazy" },
+  command = {
+    "Barbecue"
+  },
   dependencies = {
     "SmiteshP/nvim-navic",
-    "nvim-tree/nvim-web-devicons", -- optional dependency
+    "nvim-tree/nvim-web-devicons", -- Optional dependency
   },
   config = function()
     require("barbecue").setup({
-      theme = "catppuccin"
+      theme = "catppuccin",
+      exclude_filetypes = {
+        "gitcommit",
+        "neogit",
+        "oil",
+        "toggleterm",
+        "neo-tree",
+      },
     })
   end
 }
