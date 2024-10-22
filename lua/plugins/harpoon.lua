f139c5edbfa27ebaa67c26e6b2258578c645d2ab
hanan-a <hanan.avramovich@ex.co>
2024-10-22 18:06:38 +0300

    feat(harpoon): cleaner setup

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,56 +1,72 @@
+local function toggle_telescope(harpoon_files)
+  -- basic telescope configuration
+  local conf = require("telescope.config").values
+  local file_paths = {}
+  for _, item in ipairs(harpoon_files.items) do
+    table.insert(file_paths, item.value)
+  end
+
+  require("telescope.pickers").new({}, {
+    prompt_title = "Harpoon",
+    finder = require("telescope.finders").new_table({
+      results = file_paths,
+    }),
+    previewer = conf.file_previewer({}),
+    sorter = conf.generic_sorter({}),
+    initial_mode = "normal",
+  }):find()
+end
+
 return {
   "ThePrimeagen/harpoon",
   lazy = false,
+  name = "Harpoon",
+  cmd = { "Harpoon" },
   branch = "harpoon2",
   dependencies = { "nvim-lua/plenary.nvim" },
-  init = function()
-    local harpoon = require("harpoon")
+  keys = function()
+    local keys = {
+      {
+        "<leader>e",
+        function()
+          local harpoon = require("harpoon")
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Harpoon Quick Menu",
+      },
+      {
+        "<C-c>",
+        function()
+          require("harpoon"):list():next()
+        end,
+        desc = "Navigate to next Harpoon file",
+      },
+      {
+        "<C-x>",
+        function()
+          require("harpoon"):list():prev()
+        end,
+        desc = "Navigate to previous Harpoon file",
+      },
+      {
+        "<C-e>",
+        function()
+          toggle_telescope(require("harpoon"):list())
+        end,
+        desc = "Open Harpoon list with Telescope",
+      }
+    }
 
-    -- REQUIRED
-    harpoon:setup()
-    -- REQUIRED
-
-    vim.keymap.set("n", "<leader>m", function() harpoon:list():add() end)
-    vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-    -- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-    -- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-    -- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-    -- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-
-    -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set("n", "<C-x>", function() harpoon:list():prev() end)
-    vim.keymap.set("n", "<C-c>", function() harpoon:list():next() end)
-
-    -- map('n', '<c-m>', ':lua require("harpoon.ui").toggle_quick_menu()<cr>', opts)
-    -- vim.keymap.set('n', '<leader>e', ui.toggle_quick_menu, opts)
-    -- vim.keymap.set('n', '<leader>m', mark.add_file, opts)
-    -- vim.keymap.set('n', '<leader>mr', mark.rm_file, opts)
-    -- vim.keymap.set('n', '<c-c>', ui.nav_next, opts)
-    -- vim.keymap.set('n', '<c-x>', ui.nav_prev, opts)
-    --
-    -- vim.keymap.set('n', '<c-e>', ':telescope harpoon marks initial_mode=normal<cr>', opts)
-    -- vim.keymap.set('n', '<leader>e', ':telescope harpoon marks initial_mode=normal previewer=false<cr>', opts)
-    
-    -- basic telescope configuration
-    local conf = require("telescope.config").values
-    local function toggle_telescope(harpoon_files)
-      local file_paths = {}
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-      end
-
-      require("telescope.pickers").new({}, {
-        prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-          results = file_paths,
-        }),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({}),
-        initial_mode = "normal",
-      }):find()
+    for i = 1, 5 do
+      table.insert(keys, {
+        "<leader>" .. i,
+        function()
+          require("harpoon"):list():select(i)
+        end,
+        desc = "Harpoon to File " .. i,
+      })
     end
-    vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
-  end
+    return keys
+  end,
 }
 
