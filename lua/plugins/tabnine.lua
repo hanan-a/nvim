c6e2aa111b61f4fc1906b31dcfdb14b4328904ca
hanan-a <hanan.avramovich@ex.co>
2024-10-24 22:27:30 +0300

    refactor(tabnine): no config

--- lua/plugins/tabnine.lua
+++ lua/plugins/tabnine.lua
@@ -4,17 +4,15 @@ local M = {
   enabled = false,
 };
 
-M.config = function()
-  require('tabnine').setup({
-    disable_auto_comment=true,
-    accept_keymap="<C-y>",
-    dismiss_keymap = "<C-]>",
-    debounce_ms = 800,
-    suggestion_color = {gui = "#808080", cterm = 244},
-    exclude_filetypes = {"TelescopePrompt", "NvimTree"},
-    log_file_path = nil, -- absolute path to Tabnine log file
-  })
-end
+M.opts = {
+  disable_auto_comment = true,
+  accept_keymap = "<C-y>",
+  dismiss_keymap = "<C-]>",
+  debounce_ms = 800,
+  suggestion_color = { gui = "#808080", cterm = 244 },
+  exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+  log_file_path = nil,   -- absolute path to Tabnine log file
+};
 
 M.init = function()
   --- Example integration with Tabnine and LuaSnip; falling back to inserting tab if neither has a completion
