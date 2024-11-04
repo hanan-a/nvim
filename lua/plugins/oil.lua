df46223d2017d785bfe243ea024c7d429ebd728c
hanan-a <hanan.avramovich@ex.co>
2024-11-04 15:20:01 +0200

    feat(oil): "q" for quite

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,10 +1,13 @@
 return   {
   'stevearc/oil.nvim',
   opts = {
+    keymaps = {
+      ["q"] = "actions.close",
+    },
     -- configuration for the floating window in oil.open_float
     float = {
       -- padding around the floating window
-      padding = 2,
+      padding = 5,
       max_width = 0,
       max_height = 0,
       border = "rounded",
