3add669b41bf461a7cdfb68e06427b6fa7b363e1
hanan-a <hanan.avramovich@ex.co>
2024-11-23 12:11:20 +0200

    feat(surround): use mini instead of nvim-surround

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -10,6 +10,7 @@
 
 return  {
   "kylechui/nvim-surround",
+  enabled = false,
   version = "*", -- Use for stability; omit to use `main` branch for the latest features
   event = "VeryLazy",
   opts = {},
