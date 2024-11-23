1bc38a9f1d695f7333fecbcd63e9d903d6b9587d
hanan-a <hanan.avramovich@ex.co>
2024-11-23 22:49:44 +0200

    feat(globals): created global colorscheme

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -11,3 +11,12 @@ R = function(name)
   RELOAD(name)
   return require(name)
 end
+
+COLORSCHEMES = {
+  CATPPUCCIN = "catppuccin",
+  TOKYONIGHT_NIGHT = "tokyonight-night",
+}
+
+GET_COLORSCHEME = function()
+  return COLORSCHEMES.TOKYONIGHT_NIGHT
+end
