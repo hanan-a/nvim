1c07436ba51d40ec90fe9b35a5d6b52f2cf468d7
hanan-a <hanan.avramovich@ex.co>
2024-11-29 21:12:41 +0200

    feat(render-markdown): enabled

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,5 +1,6 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
+  enabled = true,
   ft = 'markdown',
   opts = {},
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
