a4031902ee7722096fe31585e5a210ac3470374a
hanan-a <hanan.avramovich@ex.co>
2024-12-26 16:52:45 +0200

    feat(snacks): configured terminal

--- lua/config/plugins/snacks.lua
+++ lua/config/plugins/snacks.lua
@@ -13,10 +13,47 @@ return {
     statuscolumn = { enabled = true },
     words = { enabled = true },
     dashboard = { enabled = false, example = "github" },
+    terminal = {
+      bo = {
+        filetype = "snacks_terminal",
+      },
+      wo = {},
+      keys = {
+        q = "hide",
+        gf = function(self)
+          local f = vim.fn.findfile(vim.fn.expand("<cfile>"), "**")
+          if f == "" then
+            Snacks.notify.warn("No file under cursor")
+          else
+            self:hide()
+            vim.schedule(function()
+              vim.cmd("e " .. f)
+            end)
+          end
+        end,
+        term_normal = {
+          "<esc>",
+          function(self)
+            self.esc_timer = self.esc_timer or (vim.uv or vim.loop).new_timer()
+            if self.esc_timer:is_active() then
+              self.esc_timer:stop()
+              vim.cmd("stopinsert")
+            else
+              self.esc_timer:start(200, 0, function() end)
+              return "<esc>"
+            end
+          end,
+          mode = "t",
+          expr = true,
+          desc = "Double escape to normal mode",
+        },
+      },
+    },
   },
   keys = {
-    { "<leader>gg", function () Snacks.lazygit() end, "Snacks: Open lazygit" },
-    { "<leader>lg", function () Snacks.lazygit.log() end, "Snacks: Open lazygit" },
-    { "<M-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
+    { "<leader>gg", function() Snacks.lazygit() end,               "Snacks: Open lazygit" },
+    { "<leader>lg", function() Snacks.lazygit.log() end,           "Snacks: Open lazygit" },
+    { "<M-o>",      function() Snacks.terminal() end,              desc = "Toggle Terminal" },
+    { "<leader>nh", function() Snacks.notifier.show_history() end, desc = "Show notifier history" },
   }
 }
