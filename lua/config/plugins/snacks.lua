4c2939f120d5d745adc67c0fd9c667baad02deb3
hanan-a <hanan.avramovich@ex.co>
2024-12-29 15:12:23 +0200

    refactor(snacks): local Snacks to avoid warnings

--- lua/config/plugins/snacks.lua
+++ lua/config/plugins/snacks.lua
@@ -1,8 +1,8 @@
+Snacks = Snacks
 return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
-  ---@type snacks.Config
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
