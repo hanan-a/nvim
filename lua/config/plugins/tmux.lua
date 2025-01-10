22269538d79ee76afaffc76062a0f96a5ea2226b
hanan-a <hanan.avramovich@ex.co>
2025-01-10 08:05:34 +0200

    lint(tmux): fix

--- lua/config/plugins/tmux.lua
+++ lua/config/plugins/tmux.lua
@@ -2,14 +2,14 @@ return {
   'aserowy/tmux.nvim',
   opts = {
     resize = {
-        -- enables default keybindings (A-hjkl) for normal mode
-        enable_default_keybindings = true,
+      -- enables default keybindings (A-hjkl) for normal mode
+      enable_default_keybindings = true,
 
-        -- sets resize steps for x axis
-        resize_step_x = 10,
+      -- sets resize steps for x axis
+      resize_step_x = 10,
 
-        -- sets resize steps for y axis
-        resize_step_y = 10,
+      -- sets resize steps for y axis
+      resize_step_y = 10,
     }
   },
 }
