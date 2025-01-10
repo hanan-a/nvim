28e73fa4c0cb9a283e32c532fe72cec5876341b8
hanan-a <hanan.avramovich@ex.co>
2025-01-10 07:39:14 +0200

    refactor(keymaps): removed redundancies

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -9,31 +9,17 @@ map('n', '<leader>R', ':source $MYVIMRC<CR>', opts)
 -- Don't yank with x
 map('n', 'x', '"_x', opts)
 
--- EXC alternative
-
--- Buffer navigation
--- map('n', '<leader>l', ':bn<CR>', opts)
--- map('n', '<leader>h', ':bp<CR>', opts)
-
 -- Splits
 map('n', '<leader>-', '<C-w>s', opts)
 map('n', '<leader>_', '<C-w>v', opts)
 map('n', '<leader>cp', '<C-w>c', opts)
 map('n', '<leader>co', '<C-w>o', opts)
 
--- Terminal handling
-map('n', '<leader>tr', ':term<CR>', opts)
-vim.api.nvim_command('autocmd TermOpen * startinsert') -- Enter terminal mode in insert mode
-vim.api.nvim_command('autocmd TermOpen * setlocal nonumber norelativenumber') -- Hide line numbers in terminal
-vim.api.nvim_command('autocmd TermOpen * setlocal signcolumn=no') -- Hide sign column in terminal
-map('t', '<ESC>', '<C-\\><C-n>', opts) -- Exit terminal mode with ESC
-
--- -- Pane Split navigation
+-- Pane Split navigation
 map('n', '<C-h>', '<C-w>h', opts)
 map('n', '<C-j>', '<C-w>j', opts)
 map('n', '<C-k>', '<C-w>k', opts)
 map('n', '<C-l>', '<C-w>l', opts)
--- map('n', '<C-c>', '<C-w>c', opts)
 
 -- Copy to clipboard
 map('n', '<leader>y', '"+y', opts)
@@ -42,27 +28,5 @@ map('v', '<leader>y', '"+y', opts)
 map('n', '<leader>p', '"+p', opts)
 map('v', '<leader>p', '"+p', opts)
 
--- Diagnostic keymaps
--- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
--- vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
--- vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
--- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
-
 -- LSP Commands
 map('n', '<leader>rlsp', ':LspRestart<CR>', opts)
-
--- -- Decrease update time
--- vim.o.updatetime = 250
--- vim.wo.signcolumn = 'yes'
-
--- Set completeopt to have a better completion experience
--- vim.o.completeopt = 'menuone,noselect'
-
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
--- plugin/terminal.lua
+++ plugin/terminal.lua
@@ -1,23 +1,27 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
--- Terminal handling
 map('n', '<leader>tr', ':term<CR>', opts)
-vim.api.nvim_command('autocmd TermOpen * startinsert') -- Enter terminal mode in insert mode
-vim.api.nvim_command('autocmd TermOpen * setlocal nonumber norelativenumber') -- Hide line numbers in terminal
-vim.api.nvim_command('autocmd TermOpen * setlocal signcolumn=no') -- Hide sign column in terminal
 map('t', '<ESC><ESC>', '<C-\\><C-n>', opts) -- Exit terminal mode with ESC
 
--- local pterminal_group = vim.api.nvim_create_augroup('PTerm', { clear = true })
--- local terminal_buf = nil
--- vim.api.nvim_create_autocmd('TermOpen', {
---   callback = function()
---     if terminal_buf == nil then
---       terminal_buf = vim.api.nvim_get_current_buf()
---     else
---       vim.api.nvim_buf_set_option(terminal_buf, 'buflisted', false)
---     end
---   end,
---   group = pterminal_group,
---   pattern = '*',
--- })
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = vim.api.nvim_create_augroup('PTerm', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
+
+local job_id = nil
+vim.keymap.set("n", "<leader>pt", function ()
+  if job_id ~= nil then
+    -- hide the split
+    vim.cmd("q")
+  else
+    vim.cmd.vnew()
+    vim.cmd.term()
+    vim.cmd.wincmd("J")
+    vim.api.nvim_win_set_height(0, 10)
+    job_id = vim.bo.channel
+  end
+end)
