345bc107c7a2693c17c2a28b321ffc3117ffbccb
Hanan Avramovich <hananavramovich@gmail>
2025-07-05 10:31:26 +0300

    feat(diffview): added keybindings for diffview

--- lua/config/plugins/diffview.lua
+++ lua/config/plugins/diffview.lua
@@ -1,4 +1,9 @@
 return {
   'sindrets/diffview.nvim',
   lazy = 'VeryLazy',
+  keys = {
+    { '<leader>dv', '<cmd>DiffviewOpen<cr>', desc = 'Open Diffview' },
+    { '<leader>dc', '<cmd>DiffviewClose<cr>', desc = 'Close Diffview' },
+    { '<leader>gh', '<cmd>DiffviewFileHistory<cr>', desc = 'File History' },
+  },
 }
