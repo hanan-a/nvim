85492d389fe63243577610b4e58734c631690255
Hanan Avramovich <hananavramovich@gmail>
2025-12-09 09:18:05 +0200

    fix(git-worktree): telescope extension

--- lua/config/plugins/git-worktree.lua
+++ lua/config/plugins/git-worktree.lua
@@ -1,48 +1,44 @@
 return {
-  'ThePrimeagen/git-worktree.nvim',
+  'polarmutex/git-worktree.nvim',
+  version = '^2',
   dependencies = {
+    "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim", -- optional
   },
   lazy = true,
-  keys = function ()
+  keys = function()
     local keys = {
-    { '<leader>gw', function() require('telescope').extensions.git_worktree.git_worktrees() end, desc = "Git Worktree: Switch" },
---     -- Creates a worktree.  Requires the path, branch name, and the upstream
--- -- Example:
--- :lua require("git-worktree").create_worktree("feat-69", "master", "origin")
---
--- -- switches to an existing worktree.  Requires the path name
--- -- Example:
--- :lua require("git-worktree").switch_worktree("feat-69")
---
--- -- deletes to an existing worktree.  Requires the path name
--- -- Example:
--- :lua require("git-worktree").delete_worktree("feat-69")
+      { '<leader>gw', function() require('telescope').extensions.git_worktree.git_worktree() end, desc = "Git Worktree: Switch" },
+
+      --     -- Creates a worktree.  Requires the path, branch name, and the upstream
+      -- -- Example:
+      -- :lua require("git-worktree").create_worktree("feat-69", "master", "origin")
+      --
+      -- -- switches to an existing worktree.  Requires the path name
+      -- -- Example:
+      -- :lua require("git-worktree").switch_worktree("feat-69")
+      --
+      -- -- deletes to an existing worktree.  Requires the path name
+      -- -- Example:
+      -- :lua require("git-worktree").delete_worktree("feat-69")
     }
 
     return keys
   end,
   opts = {},
-  config = function ()
+  config = function()
     require('telescope').load_extension('git_worktree')
+    local Hooks = require("git-worktree.hooks")
+    local config = require('git-worktree.config')
+    local update_on_switch = Hooks.builtins.update_current_buffer_on_switch
 
-    local Worktree = require("git-worktree")
-    -- op = Operations.Switch, Operations.Create, Operations.Delete
-    -- metadata = table of useful values (structure dependent on op)
-    --      Switch
-    --          path = path you switched to
-    --          prev_path = previous worktree path
-    --      Create
-    --          path = path where worktree created
-    --          branch = branch name
-    --          upstream = upstream remote name
-    --      Delete
-    --          path = path where worktree deleted
+    Hooks.register(Hooks.type.SWITCH, function(path, prev_path)
+      vim.notify("Moved from " .. prev_path .. " to " .. path)
+      update_on_switch(path, prev_path)
+    end)
 
-    Worktree.on_tree_change(function(op, metadata)
-      if op == Worktree.Operations.Switch then
-        print("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
-      end
-end)
+    Hooks.register(Hooks.type.DELETE, function()
+      vim.cmd(config.update_on_change_command)
+    end)
   end
 }
